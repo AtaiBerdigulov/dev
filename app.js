382838c9f2e8f4710df3a01bc8b44d9077aec061
AtaiBerdigulov <BerdigulovAtai@gmail.com>
2024-05-15 10:04:49 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// const brohter='Atai'
+// const father='Bekmyrza'
+// const grandfather='Berdigul'
+
+const family=['Atai', 'Bekmyrza', 'Berdigul']
+
+console.log(family[1]);
+
+family.unshift('Salman')
+console.log(family);
+
+const student={
+    name:'Mariam',
+    age:12,
+    isBishkek:true,
+    favMoviea:['yarkaya plamya', 'kr butony',
+    'Osnovaie Osmon', 'Dirilish Ertugnul']
+}
+console.log(studamt.favMoviea[0]);
+delete student.name
+console.log(studant);
